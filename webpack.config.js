4d7672093de60b9a8dd109f83dcb01f242225c68
Henning Janßen <development@henning-janssen.net>
2018-02-04 20:37:41 +0100

    Rename router to app

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ jspath = path.resolve(__dirname, 'src')
 
 module.exports = {
   entry: {
-    app: path.resolve(jspath, 'router.js')
+    app: path.resolve(jspath, 'app.js')
   },
   output: {
     path: path.resolve(__dirname, 'public', 'bundle'),
