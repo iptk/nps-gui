8e54110649ba3321b3618e774177219626259b7e
Henning Janßen <henning.janssen@ukmuenster.de>
2018-02-15 12:52:20 +0100

    DatasetTable: non-empty tablehead, handle non-existent keys, fixes

--- src/lib/dom/DatasetTable.js
+++ src/lib/dom/DatasetTable.js
@@ -4,18 +4,18 @@ import {Table, TableHead, TableRow, TableCell} from 'react-toolbox'
 import {Dataset} from '../api/dataset.js'
 
 const DatasetTable = ({datasets, keys = []}) => {
-  if(keys.length = 0 && datasets.length > 0){
+  if(keys.length == 0 && datasets.length > 0){
     for(var m of datasets[0].getMetadata()){
-      keys.append(m.key)
+      keys.push(m.key)
     }
   }
-  console.log(datasets)
 
   var rows = []
   for(var ds of datasets){
     var cells = []
     for(var k of keys){
-      cells.push(<TableCell>{datasets.getMetadata(k).value}</TableCell>)
+      var meta = ds.getMetadata(k)
+      cells.push(<TableCell>{meta !== null ? meta.value : ""}</TableCell>)
     }
     rows.push(<TableRow>{cells}</TableRow>)
   }
@@ -23,7 +23,7 @@ const DatasetTable = ({datasets, keys = []}) => {
     heading="true"
   >
     <TableHead>
-      {keys.map((item, index) => (<TableCell></TableCell>))}
+      {keys.map((item, index) => (<TableCell>{item}</TableCell>))}
     </TableHead>
     {rows}
   </Table>
