5c2fe973e684fac13a14d96d50494d82073987c8
Henning Janßen <development@henning-janssen.net>
2018-03-09 13:33:42 +0100

    Fix MetaDatasetCard

--- src/lib/dom/MetaDatasetCard.js
+++ src/lib/dom/MetaDatasetCard.js
@@ -4,23 +4,21 @@ import {Table, TableHead, TableRow, TableCell} from 'react-toolbox'
 import {translate} from 'react-i18next'
 
 const MetaDatasetCard = ({metads, t}) => {
-  metadata = metads.getMetadata()
-  var rows = Object.keys(metadata).forEach((key) => (
-    <TableRow key={key+'__'+metadata[key]}>
-      <TableCell key={key}>{key}</TableCell>
-      <TableCell key={metadata[key]}>{metadata[key]}</TableCell>
+  var metadata = metads.getMetadata()
+  var rows = metadata.map((md, _) => (
+    <TableRow key={md.key+'__'+md.value}>
+      <TableCell key={'__key__'+md.key}>{md.key}</TableCell>
+      <TableCell key={'__val__'+md.value}>{md.value}</TableCell>
     </TableRow>
   ))
   return (<Card>
       <CardTitle title={metads.id}/>
       <Table>
         <TableHead>
-          <TableRow>
-            <TableCell key='__head_key'>{t('metadatasetcard.key')}</TableCell>
-            <TableCell key='__head_value'>{t('metadatasetcard.value')}</TableCell>
-          </TableRow>
+          <TableCell key='__head_key'>{t('MetaDatasetCard.key')}</TableCell>
+          <TableCell key='__head_value'>{t('MetaDatasetCard.value')}</TableCell>
         </TableHead>
-      {metarows}
+      {rows}
       </Table>
     </Card>)
 }
