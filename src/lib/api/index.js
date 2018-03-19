dbc0af5322505edc1d322c3486fbaaa8a546915d
Henning Janßen <development@henning-janssen.net>
2018-03-19 21:14:40 +0100

    api: export KeyValueMetadata under its correct name

--- src/lib/api/index.js
+++ src/lib/api/index.js
@@ -1,7 +1,7 @@
 import {Dataset} from './dataset'
-import Metadata from './metadata'
+import KeyValueMetadata from './metadata'
 import MetadataCollection from './metadatacollection'
 import MetaDataset from './metadataset'
 import {NPS} from './NPS'
 
-export {Dataset, Metadata, MetadataCollection, MetaDataset, NPS}
+export {Dataset, KeyValueMetadata, MetadataCollection, MetaDataset, NPS}
