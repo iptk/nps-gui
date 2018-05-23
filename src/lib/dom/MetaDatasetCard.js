06f60498932fc99eb30c850f0a77c13f27068c53
Henning Janßen <development@henning-janssen.net>
2018-05-23 13:12:12 +0200

    MetaDatasetCard: Add missing imports

--- src/lib/dom/MetaDatasetCard.js
+++ src/lib/dom/MetaDatasetCard.js
@@ -4,6 +4,8 @@ import Card from '@material-ui/core/Card'
 import CardHeader from '@material-ui/core/CardHeader'
 import CardContent from '@material-ui/core/CardContent'
 import Collapse from '@material-ui/core/Collapse'
+import Icon from '@material-ui/core/Icon'
+import IconButton from '@material-ui/core/IconButton'
 
 import ObjectTable from './ObjectTable'
 
