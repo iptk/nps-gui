7ebbeba7fd8ecfc3eab6eaba00449ddb060ea7af
Henning Janßen <development@henning-janssen.net>
2018-05-24 13:32:51 +0200

    searchdataset: use material-ui

--- src/searchdataset.js
+++ src/searchdataset.js
@@ -1,14 +1,17 @@
 import 'babel-polyfill' // for cross-fetch
 
 import React from 'react'
-import {Button, Input} from 'react-toolbox'
 import {connect} from 'react-redux'
-import debounce from 'lodash/debounce'
 import {translate} from 'react-i18next'
+
+import Button from '@material-ui/core/Button'
+import Icon from '@material-ui/core/Icon'
+import TextField from '@material-ui/core/TextField'
 
 import {FILTER_SINGLE_CHANGE, FILTER_GLOBAL_CHANGE, FIELDS_CHANGE, START_CHANGE, COUNT_CHANGE, fetchDataset} from './lib/actions/searchdataset'
 import {DatasetTable, Page, QueryList} from './lib/dom'
 import reducer from './lib/reducers/searchdataset'
+import {debounceWrapper} from './lib/util'
 
 const _subscribedDatasetTable = connect(
   (state) => ({datasets: state.s.dataset, keys: state.s.filter.fields})
@@ -41,22 +44,22 @@ class SearchDataset extends Page{
     this.store.dispatch({type: FIELDS_CHANGE, fields: ["PatientsName", "SeriesDescription", "AcquisitionsDate"]})
   }
 
-  applyNewFilter(type, val){
+  applyNewFilter(type, target){
     // update filters in store
-    this.store.dispatch({type: type, filter: val})
+    this.store.dispatch({type: type, filter: target.value})
 
     // fetch dataset
     this.store.dispatch(fetchDataset(this.store.getState().s.filter))
   }
 
-  applyFields(fields){
-    fields = fields.split(',')
+  applyFields(target){
+    var fields = target.value.split(',')
     this.store.dispatch({type: FIELDS_CHANGE, fields: fields})
     this.store.dispatch(fetchDataset(this.store.getState().s.filter))
   }
 
-  changeCount(count){
-    this.store.dispatch({type: COUNT_CHANGE, count: count})
+  changeCount(target){
+    this.store.dispatch({type: COUNT_CHANGE, count: target.value})
     this.store.dispatch(fetchDataset(this.store.getState().s.filter))
   }
 
@@ -69,21 +72,30 @@ class SearchDataset extends Page{
     const {classes, t} = this.props
     return super.render(
       <section>
-        <Input type="text" label={t('searchdataset.filter_single')} multiline rows={10}
-          onChange={debounce(this.applyNewFilter.bind(this, FILTER_SINGLE_CHANGE), 600)}/>
-        <Input type="text" label={t('searchdataset.filter_global')}
-          onChange={debounce(this.applyNewFilter.bind(this, FILTER_GLOBAL_CHANGE), 600)}/>
-        <Input type="text" label={t('searchdataset.fields')}
-          placeholder="PatientsName,SeriesDescription,AcquisitionsDate"
-          onChange={debounce(this.applyFields.bind(this), 600)}/>
+        <TextField type="text" label={t('searchdataset.filter_single')}
+          fullWidth multiline rows={5} margin='normal'
+          onChange={debounceWrapper(this.applyNewFilter.bind(this, FILTER_SINGLE_CHANGE), 600)}/>
+        <TextField type="text" label={t('searchdataset.filter_global')}
+          fullWidth margin='normal'
+          onChange={debounceWrapper(this.applyNewFilter.bind(this, FILTER_GLOBAL_CHANGE), 600)}/>
+        <TextField type="text" label={t('searchdataset.fields')}
+          fullWidth margin='normal'
+          defaultValue="PatientsName,SeriesDescription,AcquisitionsDate"
+          onChange={debounceWrapper(this.applyFields.bind(this), 600)}/>
+        <br/>
         <section>
-          <Input type="number" label={t('searchdataset.resultcount')}
-            onChange={debounce(this.changeCount.bind(this), 600)}
-            placeholder="10"/>
-          <Button icon='chevron_left' label={t('searchdataset.pagebackwards')}
-            onMouseUp={this.changeStart.bind(this, false)} flat/>
-          <Button icon='chevron_right' label={t('searchdataset.pageforwards')}
-            onMouseUp={this.changeStart.bind(this, true)} flat/>
+          <TextField type="number" label={t('searchdataset.resultcount')}
+            fullWidth margin='normal'
+            onChange={debounceWrapper(this.changeCount.bind(this), 600)}
+            defaultValue="10"/>
+          <Button onClick={this.changeStart.bind(this, false)} variant='flat'>
+            <Icon>chevron_left</Icon>
+            {t('searchdataset.pagebackwards')}
+          </Button>
+          <Button onClick={this.changeStart.bind(this, true)} variant='flat'>
+            {t('searchdataset.pageforwards')}
+            <Icon>chevron_right</Icon>
+          </Button>
           <_resultNums/>
         </section>
         <_subscribedQueryList/>
