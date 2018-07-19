79e9296caf24fb99e3b58dbc2d39e40563ceee69
Henning Janßen <development@henning-janssen.net>
2018-07-19 17:12:38 +0200

    Remove unused imports

--- src/lib/dom/DatasetFilesCard.js
+++ src/lib/dom/DatasetFilesCard.js
@@ -1,11 +1,7 @@
 import React from 'react'
 import {translate} from 'react-i18next'
 
-import Card from '@material-ui/core/Card'
-import CardHeader from '@material-ui/core/CardHeader'
-import Collapse from '@material-ui/core/Collapse'
 import Icon from '@material-ui/core/Icon'
-import IconButton from '@material-ui/core/IconButton'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemIcon from '@material-ui/core/ListItemIcon'
--- src/lib/dom/DatasetListCard.js
+++ src/lib/dom/DatasetListCard.js
@@ -1,9 +1,6 @@
 import React from 'react'
 import {translate} from 'react-i18next'
 
-import Card from '@material-ui/core/Card'
-import CardHeader from '@material-ui/core/CardHeader'
-import Collapse from '@material-ui/core/Collapse'
 import Icon from '@material-ui/core/Icon'
 import IconButton from '@material-ui/core/IconButton'
 import List from '@material-ui/core/List'
