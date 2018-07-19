d88b99d1af7f441628ba5789541575d0f2b0aeb1
Henning Janßen <development@henning-janssen.net>
2018-07-19 15:34:21 +0200

    Add methods to add/remove datasets to/from comparison

--- src/lib/reducers/_common.js
+++ src/lib/reducers/_common.js
@@ -1,12 +1,15 @@
 import {
   G_START_LOADING,
   G_STOP_LOADING,
-  G_RECEIVE_METADATA_ALIASES
+  G_ADD_DATASETS_TO_COMPARISON,
+  G_RECEIVE_METADATA_ALIASES,
+  G_REMOVE_DATASETS_FROM_COMPARISON
 } from '../actions/_common'
 
 const init = {
   loading: 0,
-  metadataAliases: {}
+  metadataAliases: {},
+  datasetCompare: []
 }
 const commonReducer = (state=init, action) => {
   switch(action.type){
@@ -22,11 +25,47 @@ const commonReducer = (state=init, action) => {
         loading: state.loading > 0 ?state.loading-1 :0
       }
 
+    case G_ADD_DATASETS_TO_COMPARISON:
+      var comp = state.datasetCompare
+      var changed = false
+      if(!Array.isArray(action.dsids)){
+        action.dsids = [action.dsids]
+      }
+      action.dsids.forEach((id) => {
+        if(!comp.includes(id)){
+          comp.push(id)
+          changed = true
+        }
+      })
+      return {
+        ...state,
+        datasetCompare: changed ?[...comp] :comp
+      }
+
     case G_RECEIVE_METADATA_ALIASES:
       return {
         ...state,
         metadataAliases: action.aliases
       }
+
+    case G_REMOVE_DATASETS_FROM_COMPARISON:
+      var comp = state.datasetCompare
+      var changed = false
+      if(!Array.isArray(action.dsids)){
+        action.dsids = [action.dsids]
+      }
+      action.dsids.forEach((id) => {
+        var idx = comp.indexOf(id)
+        if(idx !== -1){
+          comp.splice(idx, 1)
+          changed = true
+        }
+      })
+      return {
+        ...state,
+        datasetCompare: changed ?[...comp] :comp
+      }
+
     default:
       return state
   }
