bd678b983146f282b746dcbb0e7fdce2a1976a03
Henning Janßen <development@henning-janssen.net>
2018-08-07 15:30:32 +0200

    Form for charts in MetaDatasetComparison

--- src/lib/dom/MetaDatasetComparisonCard.js
+++ src/lib/dom/MetaDatasetComparisonCard.js
@@ -1,16 +1,34 @@
 import React from 'react'
 import {translate} from 'react-i18next'
+import {withStyles} from '@material-ui/core/styles';
 
 import Button from '@material-ui/core/Button'
+import Divider from '@material-ui/core/Divider'
 import Icon from '@material-ui/core/Icon'
+import MenuItem from '@material-ui/core/MenuItem'
 import Table from '@material-ui/core/Table'
 import TableBody from '@material-ui/core/TableBody'
 import TableCell from '@material-ui/core/TableCell'
 import TableHead from '@material-ui/core/TableHead'
 import TableRow from '@material-ui/core/TableRow'
+import TextField from '@material-ui/core/TextField'
 import Tooltip from '@material-ui/core/Tooltip'
 
+import {ChartType} from './charts'
 import CollapsableCard from './CollapsableCard'
+
+const styles = theme => ({
+  textFieldContainer: {
+    display: 'flex',
+    flexWrap: 'wrap'
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: '30%',
+    minWidth: '200'
+  }
+})
 
 class CompTable extends React.PureComponent{
   render(){
@@ -83,13 +101,136 @@ class CompTable extends React.PureComponent{
   }
 }
 
-class MetaDatasetComparisonCard extends CollapsableCard{
+class CompChartSectionFields extends React.PureComponent{
+  constructor(props){
+    super(props)
+    this.state = {
+      chartType: 'LINE_CHART',
+      xKey: '__dsid_',
+      yKey: '__dsid_'
+    }
+  }
+  changeType(evt){
+    this.setState({
+      chartType: evt.target.value
+    })
+    if(this.props.onChangeType){
+      this.props.onChangeType(evt.target.value)
+    }
+  }
+  changeAxis = axis => evt => {
+    this.setState({
+      [axis+'Key']: evt.target.value
+    })
+    if(this.props.onChangeAxis){
+      this.props.onChangeAxis(axis, evt.target.value)
+    }
+  }
+
+  getUsableKeys(obj){
+    // totally wrong, but placeholder
+    return Object.keys(Object.flatten(obj)).map(k => ({
+      label: k, type: 'string'
+    }))
+  }
+
   render(){
-    return super.render(
-      this.props.metaid,
-      <CompTable {...this.props}/>
+    var {classes, datasets, metaid, t} = this.props
+
+    // TODO: __dsid_ => t('....dsid')
+    var keys = {'__dsid_': {label: '__dsid_', type: 'dsid'}}
+    for(var ds of datasets){
+      if(metaid in ds.metadatasets){
+        var f = this.getUsableKeys(ds.metadatasets[metaid].metadata)
+        // we need to do this for every dataset as some keys might be missing
+        // in some datasets. The complexity is horrible :(
+        for(var mkey of f){
+          if(!(mkey.label in keys)){
+            keys[mkey.label] = mkey
+          }
+        }
+      }
+    }
+    this.state[keys] = keys
+
+    var keyItems = Object.keys(keys).map(k => (
+      <MenuItem key={k} value={k}>{keys[k].label}</MenuItem>
+    ))
+
+    return (
+      <section className={classes.textFieldContainer}>
+        <TextField select
+          label={t('MetaDatasetComparisonCard.chart.charttype')}
+          onChange={this.changeType.bind(this)}
+          margin="normal"
+          value={this.state.chartType}
+          className={classes.textField}
+        >
+          {Object.keys(ChartType).map(ct => (
+            <MenuItem key={ct} value={ct}>
+              {t(ChartType[ct].label)}
+            </MenuItem>
+          ))}
+        </TextField>
+        <TextField select
+          label={t('MetaDatasetComparisonCard.chart.xaxis')}
+          value={this.state.xKey}
+          margin="normal"
+          onChange={this.changeAxis('x')}
+          className={classes.textField}
+        >
+          {keyItems}
+        </TextField>
+        <TextField select
+          label={t('MetaDatasetComparisonCard.chart.yaxis')}
+          value={this.state.yKey}
+          margin="normal"
+          onChange={this.changeAxis('y')}
+          className={classes.textField}
+        >
+          {keyItems}
+        </TextField>
+      </section>
     )
   }
 }
 
-export default translate('dom')(MetaDatasetComparisonCard)
+class CompChartSection extends React.PureComponent{
+  constructor(props){
+    super(props)
+    this.state = {
+      chartType: 'LINE_CHART',
+      xKey: '__dsid_',
+      yKey: '__dsid_'
+    }
+  }
+
+  render(){
+    var {classes, datasets, t} = this.props
+
+    return (
+      <section>
+        <CompChartSectionFields {...this.props}/>
+      </section>
+    )
+  }
+}
+
+class MetaDatasetComparisonCard extends CollapsableCard{
+  render(){
+    return super.render(
+      this.props.metaid,
+      <div>
+        <CompTable {...this.props}/>
+        <Divider/>
+        <br/>
+        <CompChartSection {...this.props}/>
+        <br/>
+      </div>
+    )
+  }
+}
+
+export default translate('dom')(
+  withStyles(styles)(MetaDatasetComparisonCard)
+)
---
+++ src/lib/dom/charts/ChartType.js
@@ -0,0 +1,7 @@
+const ChartType = Object.freeze({
+  LINE_CHART: {label: 'charts.ChartType.line', class: ''},
+  BAR_CHART: {label: 'charts.ChartType.bar', class: ''}
+})
+
+export {ChartType}
+export default ChartType
