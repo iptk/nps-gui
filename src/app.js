89841c1e7cce64071a620cc8a067bb722c057151
Henning Janßen <development@henning-janssen.net>
2018-08-09 08:22:05 +0200

    Change color sheme

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {browserHistory, Router, Route, IndexRoute} from 'react-router'
 import {I18nextProvider} from 'react-i18next'
+import {MuiThemeProvider, createMuiTheme} from '@material-ui/core/styles'
 
 import NPS from './lib/api'
 
@@ -14,23 +15,35 @@ import {BaseLayout} from './lib/dom'
 import render_dom_delayed from './lib/dom/render_dom'
 import {i18n, history} from './lib/util'
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#4a148c'
+    },
+    secondary: {
+      main: '#d81b60'
+    }
+  }
+})
 NPS.fetchConfiguration(window.location.origin+'/conf/srvlist')
   .then(() => render_dom_delayed(
       <I18nextProvider i18n={i18n}>
         <Router history={history}>
-          <BaseLayout>
-            <Route exact path="/" component={SearchDataset}/>
-            <Route path="/search" component={SearchDataset}/>
-            <Route path="/dataset">
-              <Route path="/dataset/:dsid" component={DatasetMeta}/>
-            </Route>
-            <Route path="/datasets">
-              <Route path="/datasets/compare" component={DatasetCompare}/>
-            </Route>
-            <Route path="/metadata">
-              <Route path="/metadata/collections" component={MetadataCollections}/>
-            </Route>
-          </BaseLayout>
+          <MuiThemeProvider theme={theme}>
+            <BaseLayout>
+              <Route exact path="/" component={SearchDataset}/>
+              <Route path="/search" component={SearchDataset}/>
+              <Route path="/dataset">
+                <Route path="/dataset/:dsid" component={DatasetMeta}/>
+              </Route>
+              <Route path="/datasets">
+                <Route path="/datasets/compare" component={DatasetCompare}/>
+              </Route>
+              <Route path="/metadata">
+                <Route path="/metadata/collections" component={MetadataCollections}/>
+              </Route>
+            </BaseLayout>
+          </MuiThemeProvider>
         </Router>
       </I18nextProvider>
     )
