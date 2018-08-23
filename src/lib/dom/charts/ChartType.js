672afe0f5a247caea5f45cb540bdf18933d06849
Henning Janßen <development@henning-janssen.net>
2018-08-23 17:39:35 +0200

    Add vertical barchart-type

--- src/lib/dom/charts/ChartType.js
+++ src/lib/dom/charts/ChartType.js
@@ -7,7 +7,7 @@ import {
 
 const ChartType = Object.freeze({
   LINE_CHART: {label: 'charts.ChartType.line', class: LineSeries},
-  //BAR_CHART: {label: 'charts.ChartType.bar', class: VerticalBarSeries},
+  BAR_CHART: {label: 'charts.ChartType.bar', class: VerticalBarSeries},
   MARK_CHART: {label: 'charts.ChartType.mark', class: MarkSeries},
   LINEMARK_CHART: {label: 'charts.ChartType.linemark', class: LineMarkSeries}
 })
