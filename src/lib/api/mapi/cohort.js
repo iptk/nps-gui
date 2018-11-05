f426b5553499fd607b114a550be452e6aca2e9f2
Henning Janßen <development@henning-janssen.net>
2018-11-05 17:38:57 +0100

    mapi/cohort: plannedParticipantCount and addParticipant()

--- src/lib/api/mapi/cohort.js
+++ src/lib/api/mapi/cohort.js
@@ -4,13 +4,17 @@ import Entity from './entity'
 class Cohort extends Entity{
   apipath = 'cohort'
 
-  constructor({id, name, studyID, participantIDs, study}){
+  constructor({
+    id, name, studyID, participantIDs, participants, plannedParticipantCount, study
+  }){
     super()
     this.id = id
     this.name = name
+    this.plannedParticipantCount = plannedParticipantCount || 0
     this.studyID = studyID
     this.study = study || null
     this.participantIDs = participantIDs || []
+    this.participants = participants || []
   }
 
   static get(id, recursive, study=null){
@@ -19,6 +23,7 @@ class Cohort extends Entity{
         name: json.name,
         studyID: json.studyID,
         study: (study && study.id == json.studyID) ?study :null,
+        plannedParticipantCount: json.plannedParticipantCount,
         participantIDs: json.participantIDs
       }))
       .then(c => {
@@ -27,6 +32,17 @@ class Cohort extends Entity{
         }
         return c
       })
+  }
+
+  addParticipant(participant){
+    if(this.id){
+      participant.cohortID = this.id
+    }
+    participant.cohort = this
+    this.participants.push(participant)
+    if(participant.id){
+      this.participantIDs.push(participant.id)
+    }
   }
 
   loadParticipants(recursive=true){
@@ -41,12 +57,17 @@ class Cohort extends Entity{
   }
 
   save(recursive=true){
-    super.save({name: this.name, studyID: this.studyID})
+    super.save({
+        name: this.name,
+        plannedParticipantCount: this.plannedParticipantCount,
+        studyID: this.studyID
+      })
       .then(json => {
         this.id = json.id
         this.name = json.name
         this.studyID = json.studyID
         this.participantIDs = json.participants
+        this.plannedParticipantCount = json.plannedParticipantCount
 
         if(recursive){
           return Promise.all(this.participants.map(c => {
